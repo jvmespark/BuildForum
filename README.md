4c74ee9ccb17cb2cff4fd66f3ef03f14bd04a50a
James Park <jamesdpark1@gmail.com>
2023-06-26 22:57:05 -0400

    login

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Build Forum  
 
 ## Roadmap:  
+- Pivot to a networking / music platform?  
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,30 +1,73 @@
+
+import {React, useState, useEffect} from 'react';
+import './styles/Login.css';
+
 // login page
 function Login() {
+    const [allUsernames, setAllUsernames] = useState([]);
+
+    useEffect(()=>{
+        getUsernames()
+    },[]);
+
+    const getUsernames=async()=> {
+        try {
+            const response = await fetch('http://localhost:3001/users');
+            const json = await response.json()
+            setAllUsernames(json)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    function checkUserExists(username) {
+        // go through username array 
+        if (allUsernames.length === 0) {
+            return -1;
+        }
+        for (let i = 0; i < allUsernames.length; i++) {
+            if (allUsernames[i].username === username) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     function LoginForm(e) {
-        /*
         e.preventDefault();
         const form = e.target;
         const formData = new FormData(form);
         const formJson = Object.fromEntries(formData.entries());
 
-        let username = formJson.title
-        let password = formJson.description
+        let username = formJson.username
+        let password = formJson.password
 
         if (username && password) {
             // check if the username exists and check the password
-            // getUsers()
+            let userExists = checkUserExists(username)
+            if (userExists !== -1) {
+                // check the password
+                if (allUsernames[userExists].password === password) {
+                    // login, set to cache data
+                    console.log("login")
+                }
+                else {
+                    console.log("wrong")
+                }
+            }
+            else {
             //if not create the new user
-            fetch('http://localhost:3001/users', {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({username, password}),
-            })
+                fetch('http://localhost:3001/users', {
+                    method: 'POST',
+                    headers: {
+                    'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({username, password}),
+                })
+            }
         }
         document.getElementById('username').value = ''
         document.getElementById('password').value = ''
-        */
     }
 
     return (
@@ -32,7 +75,7 @@ function Login() {
             <form action="/">
                 <input type="submit" value="Home" />
             </form>
-            <div className="login">
+            <div className="loginBox">
                 <form onSubmit={LoginForm}>
                         <label>
                             Username: <br></br>
